cc987d342b185e75b42ce6e7300e73f579c18cba
snuchat <snuchat@gmail.com>
2018-04-16 17:07:33 +0900

    tutorial part2 done now simple chat server is up and running

--- src/testchat/settings.py
+++ src/testchat/settings.py
@@ -122,3 +122,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 ASGI_APPLICATION = 'testchat.routing.application'
+CHANNEL_LAYERS = {
+	'default' : {
+		'BACKEND' : 'channels_redis.core.RedisChannelLayer',
+		'CONFIG' :{
+			"hosts" : [('127.0.0.1', 6379)],
+		},
+	},
+}
